c65e25d1a4c82d8028788fd8cf679b584bbf7a6e
AgustinB2608 <134569835+AgustinB2608@users.noreply.github.com>
2024-08-13 18:26:49 -0300

    Version 1

---
+++ TP_Grupo_7/Trabajo Practico 1/TP1_Grupo_7/Ejercicio2Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_Grupo_7
+{
+    public partial class Ejercicio2Form : Form
+    {
+        public Ejercicio2Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
